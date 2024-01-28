4ba6e86ebf3bbd0ddcc1db9b4d3aa09d14bfea47
Yershov Artem <fortestsante@gmail.com>
2024-01-28 17:28:15 +0200

    first task resolve attempt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
 const create = async () => {
     // Write your code here 
-};
+    const path = './files/';
+    const file = 'fresh.txt';
+    const content = 'I am fresh and young';
+    try {
+        if (fs.existsSync(`${path}${file}`)) throw new Error(`FS operation failed`);
+        fs.writeFileSync(`${path}${file}`, content);
+        console.log(`${file} created successfully, ${file} content is \`${content}\``);
+    } catch (error) {
+        console.log(error.message);
+    }
 
+};
 await create();
