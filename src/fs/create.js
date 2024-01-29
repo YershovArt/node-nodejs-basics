d9a528a1a28d088f96fa147b5fe3df0cc70f6c54
Yershov Artem <fortestsante@gmail.com>
2024-01-29 20:23:54 +0200

    first tusk done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,20 @@
-import fs from 'fs';
+import fs from 'node:fs/promises';
 
 const create = async () => {
     // Write your code here 
-    const path = './files/';
-    const file = 'fresh.txt';
+    const pathFile = './files/fresh.txt';
     const content = 'I am fresh and young';
     try {
-        if (fs.existsSync(`${path}${file}`)) throw new Error(`FS operation failed`);
-        fs.writeFileSync(`${path}${file}`, content);
-        console.log(`${file} created successfully, ${file} content is \`${content}\``);
+        await fs.stat(pathFile);
+        throw new Error;
     } catch (error) {
-        console.log(error.message);
-    }
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(pathFile, content);
+            console.log('file was created');
+        } else {
+            throw new Error('FS operation failed');
+        }
 
+    }
 };
 await create();
