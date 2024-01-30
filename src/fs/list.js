012fada7dab238f1aa97e18bc9631b0193f10eb0
Yershov Artem <fortestsante@gmail.com>
2024-01-30 02:08:31 +0200

    commit beafre checkout

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,4 +1,7 @@
+import fs from 'fs/promises';
+// list.js - implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)
 const list = async () => {
+
     // Write your code here 
 };
 
