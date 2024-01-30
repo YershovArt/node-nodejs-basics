340d2f21eb7006cd68d2fb9f28839f31226a4daf
Yershov Artem <fortestsante@gmail.com>
2024-01-30 22:13:20 +0200

    cli done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+// args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    // process.argv[0] 'C:\\nvm\\nvm\\v20.11.0\\node.exe',
+    // process.argv[1] ''D:\\Backend\\RsSchool\\1_NodejsBasics\\node-nodejs-basics\\src\\cli\\args.js',',
+    let i = 2
+    let out = ''
+    while (i < process.argv.length) {
+        out += `${process.argv[i].slice(2)} is ${process.argv[i + 1]}, `;
+        i += 2;
+    }
+    console.log(out);
 };
-console.log('loh');
+
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+// env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 const parseEnv = () => {
     // Write your code here 
+    let out = ''
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) out += `${key}=${process.env[key]}; `;
+    }
+    console.log(out);
 };
 
 parseEnv();
