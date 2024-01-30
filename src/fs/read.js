07333c2f0f651ba74a6ff8ed4e30aa9f022cf8a9
Yershov Artem <fortestsante@gmail.com>
2024-01-30 20:23:39 +0200

    read.js done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+// read.js - implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
 const read = async () => {
     // Write your code here 
+    const fileRead = './files/fileToRead.txt';
+    try {
+        await fs.stat(fileRead);
+        //  { encoding: 'utf-8' } if not will return buffer 
+        const fileData = await fs.readFile(fileRead, { encoding: 'utf-8' });
+        console.log(fileData);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
