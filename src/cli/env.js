2e1ffd6a2782720336e86fcb9caa8c7ae18e558c
Yershov Artem <fortestsante@gmail.com>
2024-01-31 21:29:40 +0200

    odules done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,7 @@
 // env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 const parseEnv = () => {
     // Write your code here 
-    let out = ''
+    let out = '';
     for (const key in process.env) {
         if (key.startsWith('RSS_')) out += `${key}=${process.env[key]}; `;
     }
