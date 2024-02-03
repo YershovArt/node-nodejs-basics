f498c28c01868bbbc9519f542d7653efafd8d264
Yershov Artem <fortestsante@gmail.com>
2024-02-03 08:48:23 +0200

    hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import { createHash } from 'crypto';
+import { error } from 'console';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const hash = createHash('sha256');
+    const input = fs.createReadStream('./files/fileToCalculateHashFor.txt');
+
+    input.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    input.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+
+    input.on('error', () => {
+        console.log('Error', error);
+    });
+    
 };
 
 await calculateHash();
