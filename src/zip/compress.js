6b7538572e739a6d5b6e29605c66a0dd6580ffe6
Yershov Artem <fortestsante@gmail.com>
2024-02-03 10:45:02 +0200

    zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline as pipelinePromise } from 'stream/promises';
+// compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+
+
 const compress = async () => {
     // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream('./files/fileToCompress.txt');
+    const dest = createWriteStream('./files/archive.gz');
+    try {
+        await pipelinePromise(
+            source,
+            gzip,
+            dest
+        )
+    } catch (error) {
+        console.log('Compression failed', error);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline as pipelinePromise } from 'stream/promises';
+
 const decompress = async () => {
     // Write your code here 
+    const gzip = createGunzip();
+    const source = createReadStream('./files/archive.gz');
+    const dest = createWriteStream('./files/fileToCompress.txt');
+    try {
+        await pipelinePromise(
+            source,
+            gzip,
+            dest
+        )
+    } catch (error) {
+        console.log('Compression failed', error);
+    }
 };
 
 await decompress();
